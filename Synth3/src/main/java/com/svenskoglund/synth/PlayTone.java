39b3cb2922272003218fbfa3b9192cbda00420ca
Sven Skoglund <svenrskoglund@gmail.com>
2018-05-01 08:02:02 -0600

    Trying new way of adding vertical change to osc frequency :?:

--- Synth3/src/main/java/com/svenskoglund/synth/PlayTone.java
+++ Synth3/src/main/java/com/svenskoglund/synth/PlayTone.java
@@ -72,7 +72,7 @@ public class PlayTone {
 			myFilter.frequency.set(Math.abs((mp.readScaledGyroscopeValues()[0] * 50 + 500)));
 			;
 			// myFilter.set.set(mp.readScaledGyroscopeValues()[1] + 1);
-			vertChange += .5 * (mp.readScaledAccelerometerValues()[0]) * (mp.readScaledAccelerometerValues()[0]) * 500;
+			vertChange += (mp.readScaledAccelerometerValues()[0]) * (mp.readScaledAccelerometerValues()[0]) * 1000;
 			osc.frequency.set(vertChange + 400);
 			System.out.println(osc.frequency.get());
 			// System.out.println(mp.readScaledAccelerometerValues()[0] + " " +
